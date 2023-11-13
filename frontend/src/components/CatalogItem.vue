3ea50bef95fca84884318f3722097e0f754ed12b
Dima_D <dimas_d1337@mail.ru>
2023-11-13 15:00:12 +0300

    new catalog and select

---
+++ frontend/src/components/CatalogItem.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({})
+export default class CatalogItem extends Vue {
+
+}
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
